83c2f74657dabba0bb501976fbd2be9ee3490b4e
Alexia Calazans <alexia.calazans@b2wdigital.com>
2022-02-09 15:09:52 -0300

    Create BoardGame without css

--- main.js
+++ main.js
@@ -1,21 +1,11 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/elements/base.css";
 import "./src/styles/generic/reset.css";
-
-import CardGame from "./src/components/CardGame";
 import PlayerName from "./src/components/PlayerName";
+import BoardGame from "./src/objects";
 
 const root = document.querySelector("#root");
-const htmlCardGame = CardGame("Player 1", "Player 2");
+const htmlBoardGame = BoardGame(6);
 const htmlPlayerName = PlayerName();
 
-root.insertAdjacentHTML(
-  "beforeend",
-  htmlPlayerName +
-    htmlCardGame +
-    htmlCardGame +
-    htmlCardGame +
-    htmlCardGame +
-    htmlCardGame +
-    htmlCardGame
-);
+root.insertAdjacentHTML("beforeend", htmlPlayerName + htmlBoardGame);
