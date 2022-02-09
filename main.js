2d976439d6d1daebd8b52a22e652e975789cfc45
Alexia Calazans <alexia.calazans@b2wdigital.com>
2022-02-09 14:14:09 -0300

    blabla

--- main.js
+++ main.js
@@ -3,8 +3,19 @@ import "./src/styles/elements/base.css";
 import "./src/styles/generic/reset.css";
 
 import CardGame from "./src/components/CardGame";
+import PlayerName from "./src/components/PlayerName";
 
 const root = document.querySelector("#root");
-const htmlCardGame = CardGame();
+const htmlCardGame = CardGame("Player 1", "Player 2");
+const htmlPlayerName = PlayerName();
 
-root.insertAdjacentHTML("beforeend", htmlCardGame);
+root.insertAdjacentHTML(
+  "beforeend",
+  htmlPlayerName +
+    htmlCardGame +
+    htmlCardGame +
+    htmlCardGame +
+    htmlCardGame +
+    htmlCardGame +
+    htmlCardGame
+);
---
+++ src/components/PlayerName/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+function PlayerName() {
+  return `<h1 class="player-name"> Player 1 </h1>`;
+}
+
+export default PlayerName;
