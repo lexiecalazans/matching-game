4a84f6eed3480546fc122559240f0ef39195de3a
Alexia Calazans <alexia.calazans@b2wdigital.com>
2022-02-15 11:29:13 -0300

    :art: add max width to body

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./src/styles/settings/colors.css";
 import "./src/styles/elements/base.css";
 import "./src/styles/generic/reset.css";
 import PlayerName from "./src/components/PlayerName";
-import BoardGame from "./src/objects";
+import BoardGame from "./src/objects/BoardGame";
 
 const root = document.querySelector("#root");
 const htmlBoardGame = BoardGame(6);
